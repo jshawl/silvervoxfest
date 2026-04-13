5123ab7799479d003bc464daaca618339497efd4
Jesse Shawl <jesse@jesse.sh>
2026-04-13 06:26:57 -0400

    refactor

--- includes/blocks/map/assets/view.js
+++ includes/blocks/map/assets/view.js
@@ -5,49 +5,52 @@ import { initializeFilter } from "@places/filter";
 export const getLocationTypes = (locations) =>
   [...new Set(locations.map((l) => l.type))].sort((a, b) => a.localeCompare(b));
 
-let markers = [];
-let locations;
-initializeMap({
-  onMapLoad: async (map) => {
-    const response = await fetch("/wp-json/wp/v2/place?per_page=100");
-    locations = await response.json();
-    markers = createMarkers({ locations });
-    markers.forEach(({ marker }) => marker.addTo(map));
-    fitBounds({ map, locations });
-    const tree = getLocationTypes(locations).map((type) => ({
-      id: type,
-      // TODO this is not alphabetically sorted because the emoji is in the label
-      label: `${ICON_MAP[type].emoji} ${ICON_MAP[type].label}`,
-      children: markers
-        .filter(({ location }) => location.type === type)
-        .map(({ marker, location }) => ({
-          id: location.id,
-          label: location.title.rendered,
-          marker,
-          location,
-        })),
-    }));
-    initializeFilter({
-      tree,
-      onSelect: (places) => {
-        const locations = places.map(({ location }) => location);
-        fitBounds({ map, locations });
+const buildTree = ({ locations, markers }) =>
+  getLocationTypes(locations).map((type) => ({
+    id: type,
+    // TODO this is not alphabetically sorted because the emoji is in the label
+    label: `${ICON_MAP[type].emoji} ${ICON_MAP[type].label}`,
+    children: markers
+      .filter(({ location }) => location.type === type)
+      .map(({ marker, location }) => ({
+        id: location.id,
+        label: location.title.rendered,
+        marker,
+        location,
+      })),
+  }));
 
-        markers.forEach(({ marker }) => {
-          marker.getElement().style.display = "none";
-        });
-        places.forEach(({ marker }) => {
-          marker.getElement().style.display = "block";
-        });
+const onMapLoad = async (map) => {
+  const response = await fetch("/wp-json/wp/v2/place?per_page=100");
+  const locations = await response.json();
+  const markers = createMarkers({ locations });
+  markers.forEach(({ marker }) => marker.addTo(map));
+  fitBounds({ map, locations });
+  const tree = buildTree({ locations, markers });
+  initializeFilter({
+    tree,
+    onSelect: (places) => {
+      closePopups();
+      markers.forEach(({ marker }) => {
+        marker.getElement().style.display = "none";
+      });
+      places.forEach(({ marker }) => {
+        marker.getElement().style.display = "block";
+      });
 
-        closePopups();
-        if (places.length === 1) {
-          const popup = places[0].marker.getPopup();
-          if (!popup.isOpen()) {
-            places[0].marker.togglePopup();
-          }
+      if (places.length === 1) {
+        const popup = places[0].marker.getPopup();
+        if (!popup.isOpen()) {
+          places[0].marker.togglePopup();
         }
-      },
-    });
-  },
+      }
+
+      const locations = places.map(({ location }) => location);
+      fitBounds({ map, locations });
+    },
+  });
+};
+
+initializeMap({
+  onMapLoad,
 });
