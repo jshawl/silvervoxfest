851f36cafc9837f7060efad384081b1e9a3c70dd
Jesse Shawl <jesse@jesse.sh>
2026-04-18 20:51:51 -0400

    separate test setup

---
+++ bin/test-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wp-env run tests-cli --env-cwd=wp-content/plugins/export-volunteers composer update
+wp-env run tests-cli -- sudo pecl install pcov > /dev/null 2>&1
+wp-env run tests-cli bash -- -c 'echo "extension=pcov" | sudo tee /usr/local/etc/php/conf.d/99-pcov.ini > /dev/null'
+wp-env run tests-cli bash -- -c 'echo "pcov.enabled=1" | sudo tee -a /usr/local/etc/php/conf.d/99-pcov.ini > /dev/null'
--- bin/test.sh
+++ bin/test.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-wp-env run tests-cli --env-cwd=wp-content/plugins/export-volunteers composer update
-wp-env run tests-cli -- sudo pecl install pcov > /dev/null 2>&1
-wp-env run tests-cli bash -- -c 'echo "extension=pcov" | sudo tee /usr/local/etc/php/conf.d/99-pcov.ini > /dev/null'
-wp-env run tests-cli bash -- -c 'echo "pcov.enabled=1" | sudo tee -a /usr/local/etc/php/conf.d/99-pcov.ini > /dev/null'
 wp-env run tests-cli --env-cwd=wp-content/plugins/export-volunteers vendor/bin/phpunit --coverage-html coverage/php
