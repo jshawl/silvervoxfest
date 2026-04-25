cc7608a48b9e2491b06cba3ad7d54bddde452328
Jesse Shawl <jesse@jesse.sh>
2026-04-25 05:54:13 -0400

    dry template (#14)

--- plugins/places/includes/blocks/map/render.php
+++ plugins/places/includes/blocks/map/render.php
@@ -5,16 +5,5 @@
 ?>
 
 <div <?php echo get_block_wrapper_attributes() ?>>
-  <div id="map-wrapper-outer">
-    <div class="map-filter">
-      <div class="flex">
-        <input type="search" placeholder="Filter">
-        <span class="dashicons dashicons-no-alt close-filter" aria-label="Close"></span>
-      </div>
-      <div class="no-results hidden filter-item">No results.</div>
-    </div>
-    <div id="map-wrapper-inner">
-      <div id="map"></div>
-    </div>
-  </div>
+  <?php require_once plugin_dir_path(__FILE__) . "template.html"; ?>
 </div>
